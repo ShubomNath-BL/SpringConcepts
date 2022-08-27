c1cef2c0de5f105aa6c7208cc2abfb7d74d9e090
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-08-28 03:16:39 +0530

    Added: Basic program added

--- springConcepts/src/main/java/com/demo/springConcepts/SpringConceptsApplication.java
+++ springConcepts/src/main/java/com/demo/springConcepts/SpringConceptsApplication.java
@@ -2,13 +2,19 @@ package com.demo.springConcepts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.demo.springConcepts.component.DemoBeam;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello to the spring boot project");
-		SpringApplication.run(SpringConceptsApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+		DemoBeam demoBeam = context.getBean(DemoBeam.class);
+		System.out.println("Demo Bean : "+demoBeam.toString());
+		System.out.println("\n**** Example of using multiple annotation ****");
 	}
 
 }
---
+++ springConcepts/src/main/java/com/demo/springConcepts/component/DemoBeam.java
@@ -0,0 +1,7 @@
+package com.demo.springConcepts.component;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoBeam {
+    public DemoBeam(){}
+}
