ca5b08b9c2c0ee47123250e998603f497193acb2
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-08-28 12:11:47 +0530

    Added: Logger has been added

--- springConcepts/src/main/java/com/demo/springConcepts/SpringConceptsApplication.java
+++ springConcepts/src/main/java/com/demo/springConcepts/SpringConceptsApplication.java
@@ -1,20 +1,24 @@
 package com.demo.springConcepts;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.demo.springConcepts.component.DemoBeam;
 
+import org.slf4j.Logger;
+
 @SpringBootApplication
 public class SpringConceptsApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
 
 	public static void main(String[] args) {
-		System.out.println("Hello to the spring boot project");
+		logger.debug("Hello to the spring boot project");
 		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
 		DemoBeam demoBeam = context.getBean(DemoBeam.class);
-		System.out.println("Demo Bean : "+demoBeam.toString());
-		System.out.println("\n**** Example of using multiple annotation ****");
+		logger.debug("Demo Bean : "+demoBeam.toString());
+		logger.debug("\n**** Example of using multiple annotation ****");
 	}
 
 }
